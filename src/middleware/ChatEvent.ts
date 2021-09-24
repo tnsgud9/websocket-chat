158b8ce9b8587ccd91353f2afeea2ce1a0597a91
tnsgud9 <tnsgud9@naver.com>
2021-09-25 02:15:43 +0900

    Update: change socket.io query parameter
    - 소켓 채팅에 사용될 쿼리 파라미터들 조정했습니다.
    - ChatEvent에 이전 채팅 기록을 가져오는 ChatEvent 타입을 하나 추가했습니다.

--- src/middleware/ChatEvent.ts
+++ src/middleware/ChatEvent.ts
@@ -3,4 +3,5 @@ export enum ChatEvent {
   DISCONNECT = "disconnect",
   NEW_MESSAGE = "new_message",
   GET_MESSAGE = "get_message",
+  GET_CHAT_HISTROY = "get_chat_history",
 }
--- src/middleware/socket.ts
+++ src/middleware/socket.ts
@@ -13,8 +13,15 @@ export const initWebSocket = (server: Server): void => {
   });
   io.on(ChatEvent.CONNECTION, (socket: webSocket.Socket) => {
     console.log("conntected: ", socket.id);
-    console.log(socket.handshake.query.param);
-    socket.join("test");
+    console.log(socket.handshake.query.user);
+    const userId = socket.handshake.query.user;
+
+    //TODO: Join 되기 전 추가적으로 DB와 함꼐 조회해서
+    //      해당 user가 있는지 검증해야함.
+
+    // TODO: 위 해당 검증후 DB에 접속하여
+    //       이전 기록에 대한 채팅 내용을 모두 전달 해줘야함.
+    socket.join(`${userId}`);
   });
   io.on(ChatEvent.DISCONNECT, (socket: webSocket.Socket) => {
     console.log("DISCONNECT: ", socket.id);
