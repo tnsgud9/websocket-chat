1581a99ae24f6feba5b7ca6539717f541d463702
tnsgud9 <tnsgud9@naver.com>
2021-09-26 16:09:08 +0900

    feat: socket chat function
    소켓 채팅 기능을 구현했습니다.

--- src/middleware/ChatEvent.ts
+++ src/middleware/ChatEvent.ts
@@ -3,5 +3,6 @@ export enum ChatEvent {
   DISCONNECT = "disconnect",
   NEW_MESSAGE = "new_message",
   GET_MESSAGE = "get_message",
+  SEND_MESSAGE = "send_message",
   GET_CHAT_HISTROY = "get_chat_history",
 }
--- src/middleware/socket.ts
+++ src/middleware/socket.ts
@@ -15,13 +15,18 @@ export const initWebSocket = (server: Server): void => {
     console.log("conntected: ", socket.id);
     console.log(socket.handshake.query.user);
     const userId = socket.handshake.query.user;
+    socket.join(`${userId}`);
+    socket.on(ChatEvent.NEW_MESSAGE, (message: string) => {
+      console.log("New Message : ", message);
+      //Verify 확인해 되면
+      io.to(`${userId}`).emit(ChatEvent.NEW_MESSAGE, message);
+    });
 
     //TODO: Join 되기 전 추가적으로 DB와 함꼐 조회해서
     //      해당 user가 있는지 검증해야함.
 
     // TODO: 위 해당 검증후 DB에 접속하여
     //       이전 기록에 대한 채팅 내용을 모두 전달 해줘야함.
-    socket.join(`${userId}`);
   });
   io.on(ChatEvent.DISCONNECT, (socket: webSocket.Socket) => {
     console.log("DISCONNECT: ", socket.id);
